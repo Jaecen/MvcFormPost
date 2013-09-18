1e1cf35753de901cc412d7fd064ddb310a76f603
Jason Addington <jason@vortx.com>
2013-09-18 09:49:53 -0700

    Initial Commit
    - Create sample form post demo.

---
+++ MvcFormPost.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace MvcFormPost.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Update()
+		{
+			var textValue = Request.Form["TextValue"];
+			var checkValue = Request.Form["CheckValue"];
+
+			ViewBag.TextValue = textValue;
+			ViewBag.CheckValue = checkValue;
+
+			return View();
+		}
+	}
+}
---
+++ MvcFormPost.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcFormPost.Web.MvcApplication" Language="C#" %>
